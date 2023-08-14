ecdacbd8e02a3b9780af4114ed5815beaaed7ad7
Erick Amorim <101286204+Amorim-EA@users.noreply.github.com>
2023-08-14 16:34:00 -0400

    Salvando o arquivo pela Primeira vez.

---
+++ sw.js
@@ -0,0 +1,18 @@
+let cacheName="Peso Ideal - IMC";
+let filesToCache = ["/", "/index.html","/css/style.css", "/js/main.js"];
+
+self.addEventListener("install", (e) => { 
+    e.waitUntil(
+       caches.open(cacheName).then(function (cache) {
+         return cache.addAll(filesToCache);
+       })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondwith(
+        caches.match(e.request).then((response) => {
+          return response || fetch(e.request); 
+        })
+    );
+});
